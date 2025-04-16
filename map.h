40da38d0a461f3ab4bc2b831fb6eee994fdb2d7d
Hatim-Mustafa <hatimmustafa108@gmail.com>
2025-04-16 17:50:06 +0500

    Map class moved

---
+++ map.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Map
+{
+private:
+    static const int ROWS = 51;
+    static const int COLS = 46;
+    static const int TILE_SIZE = 15;
+    const int grid[ROWS][COLS];
+
+public:
+    Map();
+    void draw(RenderWindow& window) const;
+    bool isWall(int row, int col) const;
+
+    // Accessors
+    static int getRows() { return ROWS; }
+    static int getCols() { return COLS; }
+    static int getTileSize() { return TILE_SIZE; }
+};
